43641c0f31c1e99bccec1f40014354b18d8d5cc1
m4rquee <lucasxm12@gmail.com>
2024-02-27 10:03:58 +0100

    in-degree constraints dualized

--- src/solver_ilp_lagran.cpp
+++ src/solver_ilp_lagran.cpp
@@ -7,21 +7,22 @@
 #include <string>
 
 #define MAX_ITER 100
-#define TOTAL_FRAC 0.9
+#define TOTAL_FRAC 0.75
 
 int seed = 42;// seed to the random number generator
 
 double lagrangian_relaxation(const Problem_Instance &P, GRBModel &model, const Digraph::ArcMap<GRBVar> &x_e,
                              const GRBVar &depth, double &Z_LB, double &Z_UB, double &min_delta) {
     // The out-degree constraints will be dualized: ----------------------------------
-    auto lambda = new double[P.nnodes], g_k = new double[P.nnodes];
+    auto in_lambda = new double[P.nnodes], in_g_k = new double[P.nnodes];
+    auto out_lambda = new double[P.nnodes], out_g_k = new double[P.nnodes];
     double pi_k = 2.0;
 
     // Compute the minimum cost difference between two solutions:
     for (ArcIt e(P.g); e != INVALID; ++e) min_delta = min(min_delta, (double) P.weight[e]);
 
     // Initialize the Lagrangian multipliers lambda:
-    for (int i = 0; i < P.nnodes; i++) lambda[i] = 0.0;
+    for (int i = 0; i < P.nnodes; i++) in_lambda[i] = out_lambda[i] = 0.0;
 
     cout << "\n→ Status before subgradient method: LB = " << Z_LB << "; UB = " << Z_UB
          << "; GAP = " << (Z_UB - Z_LB) / Z_UB * 100 << "%\n\n";
@@ -34,11 +35,16 @@ double lagrangian_relaxation(const Problem_Instance &P, GRBModel &model, const D
         GRBLinExpr obj_expr = depth;
         int index = 0;
         for (DNodeIt v(P.g); v != INVALID; ++v, index++) {
-            GRBLinExpr out_degree_expr;
+            GRBLinExpr out_degree_expr, in_degree_expr;
             for (OutArcIt e(P.g, v); e != INVALID; ++e) out_degree_expr += x_e[e];
-            if (v == P.source) obj_expr += lambda[index] * (out_degree_expr - 1);
-            else
-                obj_expr += lambda[index] * (out_degree_expr - 2);
+            for (InArcIt e(P.g, v); e != INVALID; ++e) in_degree_expr += x_e[e];
+            if (v == P.source) {
+                obj_expr += out_lambda[index] * (out_degree_expr - 1);
+                obj_expr += in_lambda[index] * (in_degree_expr - 0);
+            } else {
+                obj_expr += out_lambda[index] * (out_degree_expr - 2);
+                obj_expr += in_lambda[index] * (in_degree_expr - 1);
+            }
         }
         model.setObjective(obj_expr);
 
@@ -63,14 +69,20 @@ double lagrangian_relaxation(const Problem_Instance &P, GRBModel &model, const D
         index = 0;
         double g_k_srq_sum = 0;
         for (DNodeIt v(P.g); v != INVALID; ++v, index++) {
-            g_k[index] = v == P.source ? -1 : -2;
-            for (OutArcIt e(P.g, v); e != INVALID; ++e) g_k[index] += x_e[e].get(GRB_DoubleAttr_X);
-            g_k_srq_sum += g_k[index] * g_k[index];
+            out_g_k[index] = v == P.source ? -1 : -2;
+            in_g_k[index] = v == P.source ? -0 : -1;
+            for (OutArcIt e(P.g, v); e != INVALID; ++e) out_g_k[index] += x_e[e].get(GRB_DoubleAttr_X);
+            for (InArcIt e(P.g, v); e != INVALID; ++e) in_g_k[index] += x_e[e].get(GRB_DoubleAttr_X);
+            g_k_srq_sum += out_g_k[index] * out_g_k[index] + in_g_k[index] * in_g_k[index];
         }
 
         // Update the lagrangian multipliers:
         auto alpha_k = pi_k * (Z_UB - Z_LB_k) / g_k_srq_sum;
-        for (int i = 0; i < P.nnodes; i++) lambda[i] = max(0.0, lambda[i] + alpha_k * g_k[i]);
+        // The first (root) constraint is an equality one, and we should not take max:
+        out_lambda[0] = out_lambda[0] + alpha_k * out_g_k[0];
+        for (int i = 1; i < P.nnodes; i++) out_lambda[i] = max(0.0, out_lambda[i] + alpha_k * out_g_k[i]);
+        // The in-degree constraints are equality one, and we should not take max:
+        for (int i = 0; i < P.nnodes; i++) in_lambda[i] = in_lambda[i] + alpha_k * in_g_k[i];
 
         pi_k *= 0.9;// decrease the pi value to guarantee convergence
         if (pi_k < MY_EPS) break;
@@ -163,24 +175,6 @@ bool solve(Problem_Instance &P, double &LB, double &UB) {
     cout << "-> a node depth is at least the source distance to its father plus the distance from its father to it - "
          << constrCount << " constrs" << endl;
 
-    // The source is the root:
-    GRBLinExpr s_in_degree_expr;
-    for (InArcIt e(P.g, P.source); e != INVALID; ++e) s_in_degree_expr += x_e[e];
-    model.addConstr(s_in_degree_expr == 0);
-    cout << "-> the source is the root - " << 1 << " constrs" << endl;
-
-    // The in-degree is one for each internal node:
-    constrCount = 0;
-    for (DNodeIt v(P.g); v != INVALID; ++v) {
-        if (v == P.source) continue;
-        GRBLinExpr in_degree_expr;
-        for (InArcIt e(P.g, v); e != INVALID; ++e) in_degree_expr += x_e[e];
-        // The in-degree is one and the out-degree is at most two for each internal node:
-        model.addConstr(in_degree_expr == 1);
-        constrCount++;
-    }
-    cout << "-> the in-degree is one for each internal node - " << constrCount << " constrs" << endl;
-
     // The number of edges is n-1 for any tree:
     GRBLinExpr arc_sum_expr;
     for (ArcIt e(P.g); e != INVALID; ++e) arc_sum_expr += x_e[e];
@@ -241,22 +235,28 @@ bool solve(Problem_Instance &P, double &LB, double &UB) {
         model.setObjective((GRBLinExpr) depth);
 
         cout << "Adding the model restrictions:" << endl;
-        // The source has out-degree one:
+        // The source is the root and has out-degree one:
         GRBLinExpr s_out_degree_expr;
         for (OutArcIt e(P.g, P.source); e != INVALID; ++e) s_out_degree_expr += x_e[e];
         model.addConstr(s_out_degree_expr == 1);
-        cout << "-> the source has out-degree one - " << 1 << " constrs" << endl;
+        GRBLinExpr s_in_degree_expr;
+        for (InArcIt e(P.g, P.source); e != INVALID; ++e) s_in_degree_expr += x_e[e];
+        model.addConstr(s_in_degree_expr == 0);
+        cout << "-> the source is the root and has out-degree one - " << 2 << " constrs" << endl;
 
-        // The out-degree is at most two for each internal node:
+        // The in-degree is one and the out-degree is at most two for each internal node:
         constrCount = 0;
         for (DNodeIt v(P.g); v != INVALID; ++v) {
             if (v == P.source) continue;
-            GRBLinExpr out_degree_expr;
+            GRBLinExpr out_degree_expr, in_degree_expr;
             for (OutArcIt e(P.g, v); e != INVALID; ++e) out_degree_expr += x_e[e];
+            for (InArcIt e(P.g, v); e != INVALID; ++e) in_degree_expr += x_e[e];
             model.addConstr(out_degree_expr <= 2);
-            constrCount++;
+            model.addConstr(in_degree_expr == 1);
+            constrCount += 2;
         }
-        cout << "-> the out-degree is at most two for each internal node - " << constrCount << " constrs\n\n";
+        cout << "-> the in-degree is one and the out-degree is at most two for each internal node - " << constrCount
+             << " constrs\n\n";
 
         model.setCallback(&cb);
         depth.set(GRB_DoubleAttr_LB, Z_LB_k);// impose the best lower bound
