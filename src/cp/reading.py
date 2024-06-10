6ea34c1c0012364db8d314ba654d81b7eda1fb36
m4rquee <lucasxm12@gmail.com>
2024-06-10 11:42:24 +0200

    random clusters conection

--- src/cp/reading.py
+++ src/cp/reading.py
@@ -1,5 +1,6 @@
 from math import log
 import networkx as nx
+from random import sample
 
 
 def read_tsplib_2d_graph():
@@ -54,7 +55,7 @@ def gnp_graph(n, p, max_iter=1000):
             graph = aux
             break
     if graph is None: exit(f'Could not generate a connected graph after {max_iter} trials!')
-    return list(graph.nodes), list(graph.edges)
+    return list(graph.nodes), list(graph.edges(data=True))
 
 
 def ws_graph(n, k, p):
@@ -62,21 +63,21 @@ def ws_graph(n, k, p):
     p = float(p)
     k = int(k)
     aux = nx.connected_watts_strogatz_graph(n, k, p)
-    return list(aux.nodes), list(aux.edges)
+    return list(aux.nodes), list(aux.edges(data=True))
 
 
 def tree_graph(r, h):
     r = int(r)
     h = int(h)
     aux = nx.balanced_tree(r, h)
-    return list(aux.nodes), list(aux.edges)
+    return list(aux.nodes), list(aux.edges(data=True))
 
 
 def regular_graph(d, n):
     d = int(d)
     n = int(n)
     aux = nx.random_regular_graph(d, n)
-    return list(aux.nodes), list(aux.edges)
+    return list(aux.nodes), list(aux.edges(data=True))
 
 
 def lobster_graph(n, p1, p2):
@@ -84,18 +85,15 @@ def lobster_graph(n, p1, p2):
     p1 = float(p1)
     p2 = float(p2)
     aux = nx.random_lobster(n, p1, p2)
-    return list(aux.nodes), list(aux.edges)
+    return list(aux.nodes), list(aux.edges(data=True))
 
 
-def combine_graphs(g, h):
+def combine_graphs(g, h, k=1):
     prod = nx.empty_graph(len(g) * len(h))
-    i = 0
     for e in g.edges:
-        prod.add_edge(e[0] * len(h) + i, e[1] * len(h) + i)
-        i = (i + 1) % len(h)
-        # for v in h:
-        #     prod.add_edge(e[0] * len(h) + v, e[1] * len(h) + v)
+        for i in sample(range(len(h)), k):
+            prod.add_edge(e[0] * len(h) + i, e[1] * len(h) + i)
     for e in h.edges:
         for v in g:
             prod.add_edge(v * len(h) + e[0], v * len(h) + e[1])
-    return list(prod.nodes), list(prod.edges)
+    return list(prod.nodes), list(prod.edges(data=True))
